28146b5b3b4fa14e638a39b88134c6c0a0f2c300
Sophie <sophiebrown438@gmail.com>
2020-05-29 11:34:53 +0100

    hbd page with sessions and sinatra modular

--- app.rb
+++ app.rb
@@ -1,5 +1,24 @@
-require 'sinatra'
+require 'sinatra/base'
+
+class Birthday < Sinatra::Base
+enable :sessions
+
 
 get '/' do
   erb(:index)
 end
+
+post '/info' do
+  session[:name] = params[:name]
+  $name = session[:name]
+  p $name
+  redirect '/hbd'
+end
+
+get '/hbd' do
+  p $name
+  erb(:hbd)
+end
+end
+
+
